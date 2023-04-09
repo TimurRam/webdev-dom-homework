a68b2d830ec3589c6c182aa4db5740feaf793dcd
Ramazanov Timur <Sieger.07@mail.ru>
2023-04-09 14:13:45 +0300

    не обработаны ошибки

--- js/components/login-component.js
+++ js/components/login-component.js
@@ -1,96 +1,130 @@
-import { userLogin, userRegistration } from "../userApi.js";
+import { userLogin, userRegistration } from '../userApi.js'
 
+export function renderLoginComponent ({ appEl, setToken, renderApp, comments, setUserName }) {
+  let isLoginMode = true
+  let formMode = true
 
-export function renderLoginComponent({appEl, setToken, renderApp} ) {
-    
+  const renderForm = () => {
+    const commentHtml = comments
+      .map((comment, index) => {
+        return `<li class="comment" id = "comment-list" data-index="${index}">
+            <div class="comment-header">
+              <div data-index="${index}">${comment.name}</div>
+              <div>${comment.date} </div>
+            </div>
+            <div class="comment-body">
+            
+                <div class="comment-text"data-index = "${index}">
+                ${comment.text} 
+              </div>
+            </div>
+            <div class="comment-footer">
+              <div class="likes">
+              <button id="delete-button" class = "delete-button" data-id ="${
+                comment.id
+              }">Удалить</button>
+                <span class="likes-counter">${comment.likeCount}</span>
+                <button data-index="${index}" class="${
+          comment.liked
+            ? 'like-button -loading-like -active-like'
+            : 'like-button'
+        }"></button>
+                
+              </div>
+            </div>
+          </li>`
+      })
+      .join('')
 
-    const appHtml = `  
+    const appHtml = `<div class="container">
+        <ul class="comments" id="list" data>
+        ${commentHtml}
+          <!-- Рендеринг в JS -->
+    </ul>  
+    <section id="loaderComments" class="loader -display-none">
+    <h4 class="loader-text" id="loaderText">Комментарии загружаются...</h4>
+    <div class="loader__gif">
+        <img src="./img/Spinner-1s-100px.svg" alt="loader">
+    </div>
+    </section>
     <!-- Форма входа  -->
-   <div class="add-form-authorization">
-    <h3 class="form-text">Форма авторизации</h3>
+<div class="add-form-authorization -display none">
+    <h3 class="form-text">Форма ${
+      isLoginMode ? 'Авторизации' : 'Регистрации'
+    } </h3>
+    ${
+      isLoginMode
+        ? ''
+        : `<input id="name-input-registration" type="text" class="registration-name" placeholder="Введите Имя" />`
+    }
+    
     <input id="login-input" type="text" class="add-form-login" placeholder="Введите логин" />
     <input id="password-input" type="password" class="add-form-password" placeholder="Введите пароль" />
         <div class="add-form-row">
-      <button class="add-form-button" id="login-button">Войти</button>
-      <button class="add-form-button" id="toggle-button">Зарегистрироваться</button>
-          </div>`
-    appEl.innerHTML = appHtml;
+    <button class="add-form-button" id="login-button">${
+      isLoginMode ? 'Войти' : 'Зарегестрироваться'
+    } </button>
+    <button class="add-form-button" id="toggle-button">Перейти  ${
+      isLoginMode ? 'к регистрации' : 'ко входу'
+    } </button>
+        </div>`
+    appEl.innerHTML = appHtml
+
     document.getElementById('login-button').addEventListener('click', () => {
-        const login = document.getElementById("login-input").value;
-        const password = document.getElementById("password-input").value;
+      if (isLoginMode) {
+        const login = document.getElementById('login-input').value
+        const password = document.getElementById('password-input').value
         if (!login) {
-            alert("Введите login");
-            return;
+          alert('Введите login')
+          return
         }
         if (!password) {
-            alert("Введите password");
-            return;
+          alert('Введите password')
+          return
         }
         userLogin(login, password)
-            .then((user) => {
-                setToken(`Bearer ${user.user.token}`);
-                renderApp();
-
-            }).catch(error => {
-                alert(error.message);
-            });
-    });
-    document.getElementById('toggle-button').addEventListener('click', () => {
-
-        const appHtml = `<!-- Форма регистрации -->
-       <div class="add-form-registration">
-        <h3 class="form-text">Регистрация</h3>
-        <input id="name-input-registration" type="text" class="registration-name" placeholder="Введите Имя" />
-        <input id="login-input-registration" type="text" class="registration-login" placeholder="Введите логин" />
-        <input id="password-input-registration" type="password" class="registration-password" placeholder="Введите пароль" />
-      
-        <div class="add-form-row">
-          <button class="add-form-button" id="registration-button">Зарегистрироваться</button>
-          <button class="add-form-button" id="exit-button">К входу</button>
-         </div>`
-        appEl.innerHTML = appHtml;
-        document.getElementById('registration-button').addEventListener('click', () => {
-            const login = document.getElementById("login-input-registration").value;
-            const password = document.getElementById("password-input-registration").value;
-            const name = document.getElementById('name-input-registration').value;
-            if (!login) {
-                alert("Введите login");
-                return;
-            }
-            if (!password) {
-                alert("Введите password");
-                return;
-            }
-            if (!name) {
-                alert("Введите name");
-                return;
-            }
-            userRegistration({
-                login,
-                name,
-                password,
-            }).then((user) => {
-                setToken(`Bearer ${user.user.token}`);
-                renderApp();
-            })
-        });
-
-        const exit = document.getElementById('exit-button');
-        exit.addEventListener('click', () => {
-            const appHtml = `  
-    <!-- Форма входа  -->
-   <div class="add-form-authorization">
-    <h3 class="form-text">Форма авторизации</h3>
-    <input id="login-input" type="text" class="add-form-login" placeholder="Введите логин" />
-    <input id="password-input" type="password" class="add-form-password" placeholder="Введите пароль" />
-        <div class="add-form-row">
-      <button class="add-form-button" id="login-button">Войти</button>
-      <button class="add-form-button" id="toggle-button">Зарегистрироваться</button>
-          </div>`
-            appEl.innerHTML = appHtml;
-            renderApp();
-
+          .then(user => {
+            setToken(`Bearer ${user.user.token}`)
+            setUserName(`${user.user.name}`)
+            renderApp()
+          })
+          .catch(error => {
+            alert(error.message)
+          })
+      } else {
+        const login = document.getElementById('login-input').value
+        const password = document.getElementById('password-input').value
+        const name = document.getElementById('name-input-registration').value
+        if (!login) {
+          alert('Введите login')
+          return
+        }
+        if (!password) {
+          alert('Введите password')
+          return
+        }
+        if (!name) {
+          alert('Введите name')
+          return
+        }
+        userRegistration({
+          login,
+          name,
+          password
         })
+          .then(user => {
+            setToken(`Bearer ${user.user.token}`)
+            renderApp()
+          })
+          .catch(error => {
+            alert(error.message)
+          })
+      }
     })
-
+    document.getElementById('toggle-button').addEventListener('click', () => {
+      isLoginMode = !isLoginMode
+      renderForm()
+    })
+  }
+  renderForm()
 }
--- js/script.js
+++ js/script.js
@@ -1,250 +1,246 @@
-import { deleteComments, getComments, postComments, repeatPostApp } from "./api.js";
-import { renderLoginComponent } from "./components/login-component.js";
+import {
+  deleteComments,
+  getComments,
+  postComments,
+  repeatPostApp
+} from './api.js'
+import { renderLoginComponent } from './components/login-component.js'
 
 // const likeButtons = document.querySelectorAll(".like-button");
-const buttonElement = document.getElementById("add-button");
-const nameElement = document.getElementById("name-input");
+const buttonElement = document.getElementById('add-button')
+const nameElement = document.getElementById('name-input')
 // const commentsElement = document.getElementById("comments-input");
 // const listElement = document.getElementById("list");
-const commentListElement = document.getElementById("comment-list");
-export const mainForm = document.querySelector(".add-form");
+const commentListElement = document.getElementById('comment-list')
+export const mainForm = document.querySelector('.add-form')
 // const loaderText = document.getElementById("loaderText");
 // const loaderCommentsElement = document.getElementById("loaderComments");
 // console.log(loaderCommentsElement);
-let token = "Bearer cwboc4d0co6gckd8b8cocwdg6g39k3c03cg3d83cw3k39c3b43cg3b43bw3b43ck3co3bc";
+let token = '';
 let comments = [];
 
-fetchGetAndRenderComments();
-token = null;
+fetchGetAndRenderComments()
+let userName = "";
+console.log(userName);
 
-// GET запрос 
-export function fetchGetAndRenderComments() {
-  return getComments()
-  .then((responseData) => {
-      const options = {
+// GET запрос
+export function fetchGetAndRenderComments () {
+  return getComments().then(responseData => {
+    const options = {
       year: '2-digit',
       month: 'numeric',
       day: 'numeric',
       timezone: 'UTC',
       hour: 'numeric',
       minute: '2-digit'
-    };
-    const appComments = responseData.comments.map((comment) => {
+    }
+    const appComments = responseData.comments.map(comment => {
       return {
         id: comment.id,
         name: comment.author.name,
-        date: new Date(comment.date).toLocaleDateString("ru-RU", options).replace(',', ' '),
+        date: new Date(comment.date)
+          .toLocaleDateString('ru-RU', options)
+          .replace(',', ' '),
         text: comment.text,
         likeCount: comment.likes,
-        liked: false,
-      };
-    });
-    comments = appComments;
+        liked: false
+      }
+    })
+    comments = appComments
+
     // loaderCommentsElement.classList.add('-display-none');
-    renderApp();
+    renderApp()
     // }).catch((error) => {
     //   alert('Проверьте подключение к интернету');
     //   console.warn(error);
     // })
-  });
+  })
 }
-
-
 
 // // Кнопка лайка
-function ButtonTouch() {
-  const likeButtons = document.querySelectorAll(".like-button");
+function ButtonTouch () {
+  const likeButtons = document.querySelectorAll('.like-button')
   for (const likeButton of likeButtons) {
-    likeButton.addEventListener("click", () => {
-      const index = likeButton.dataset.index;
+    likeButton.addEventListener('click', () => {
+      const index = likeButton.dataset.index
 
       if (comments[index].liked === false) {
-        comments[index].liked = true;
-        comments[index].likeCount += 1;
-
+        comments[index].liked = true
+        comments[index].likeCount += 1
       } else {
-        comments[index].liked = false;
-        comments[index].likeCount -= 1;
+        comments[index].liked = false
+        comments[index].likeCount -= 1
       }
       delay(2000).then(() => {
-        renderApp();
-
+        renderApp()
       })
     })
-  };
+  }
 }
-function delay(interval = 300) {
-  return new Promise((resolve) => {
+function delay (interval = 300) {
+  return new Promise(resolve => {
     setTimeout(() => {
-      resolve();
-    }, interval);
-  });
+      resolve()
+    }, interval)
+  })
 }
 // Цитирует выбранный комментарий
-function initTouchComment() {
-  const touchComments = document.querySelectorAll(".comment-text");
-  const commentsElement = document.getElementById("comments-input");
+function initTouchComment () {
+  const touchComments = document.querySelectorAll('.comment-text')
+  const commentsElement = document.getElementById('comments-input')
 
   for (const comment of touchComments) {
-    comment.addEventListener("click", () => {
-      const index = comment.dataset.index;
-      commentsElement.value = `${comments[index].text}<\n>${comments[index].name}<\n`;
-
-    });
+    comment.addEventListener('click', () => {
+      const index = comment.dataset.index
+      commentsElement.value = `${comments[index].text}<\n>${comments[index].name}<\n`
+    })
   }
 }
 
 // Удаление последнего комментария
-function buttonDelete() {
-  const buttonDeleteElements = document.querySelectorAll(".delete-button");
+function buttonDelete () {
+  const buttonDeleteElements = document.querySelectorAll('.delete-button')
   for (const deleteButton of buttonDeleteElements) {
     const id = deleteButton.dataset.id
     deleteButton.addEventListener('click', () => {
       deleteButton.textContent = 'Удаляется...'
-      return deleteComments({token, id}).then((responseData) => {
-        comments = responseData;
-      }).then(() => {
-        fetchGetAndRenderComments();
-      })
+      return deleteComments({ token, id })
+        .then(responseData => {
+          comments = responseData
+        })
+        .then(() => {
+          fetchGetAndRenderComments()
+        })
     })
   }
 }
 
-
-
-
-
 // Повторный POST запрос при ошибке 500
-function repeatedRequestComment() {
-  mainForm.classList.add('-display-none');
-  loaderCommentsElement.classList.remove('-display-none');
+function repeatedRequestComment () {
+  mainForm.classList.add('-display-none')
+  loaderCommentsElement.classList.remove('-display-none')
   // Передаем комментарий
   return repeatPostApp({
     token,
-    text:commentsElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-  }).then((responseData) => {
-    comments = responseData;
-  }).then(() => {
-    return fetchGetAndRenderComments();
-
-  }).then((data) => {
-    mainForm.classList.remove('-display-none');
-    loaderCommentsElement.classList.add('-display-none');
-
-    nameElement.value = '';
-    commentsElement.value = '';
-    buttonElement.disabled = true;
-  }).catch((error) => {
-    if (error.message === 'Слишком коротко') {
-      alert("name и text должены содержать хотя бы 3 символа");
-      mainForm.classList.remove('-display-none');
-      loaderCommentsElement.classList.add('-display-none');
-
-    } if (error.message === 'Сервер сломался') {
-      repeatedRequestComment();
-      console.warn(error);
-    }
-
-
-  });
-  
-};
-
-
-
-function addComments() {
-  const buttonElement = document.getElementById("add-button");
-  const nameElement = document.getElementById("name-input");
-  const commentsElement = document.getElementById("comments-input");
-  const mainForm = document.querySelector(".add-form");
-  const loaderCommentsElement = document.getElementById("loaderComments");
-  buttonElement.disabled = true;
-
-  
-  document.querySelectorAll("#name-input,#comments-input").forEach(element => {
-
-    element.addEventListener('input', () => {
-      if (nameElement.value === '' || commentsElement.value === '') {
-        buttonElement.disabled = true;
-      } else {
-        buttonElement.disabled = false;
-      }
-    });
-  });
-
-
-  mainForm.addEventListener("keyup", (e) => {
-    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-      buttonElement.click();
-    }
-  });
-  buttonElement.addEventListener("click", () => {
-
-    nameElement.classList.remove("error");
-    commentsElement.classList.remove("error");
-
-    if (nameElement.value === '') {
-      nameElement.classList.add("error");
-      return
-
-    } else if (commentsElement.value === '') {
-      commentsElement.classList.add("error");
-      return
-    }
-    mainForm.classList.add('-display-none');
-    loaderCommentsElement.classList.remove('-display-none');
-
-    // Передаем комментарий
-   return postComments({
-    token,
-     text: commentsElement.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;"),
-    }).then((responseData) => {
-      comments = responseData;
-    }).then(() => {
-      return fetchGetAndRenderComments();
-
-    }).then(() => {
-      mainForm.classList.remove('-display-none');
-      loaderCommentsElement.classList.add('-display-none');
-
-      nameElement.value = '';
-      commentsElement.value = '';
-      buttonElement.disabled = true;
-    }).catch((error) => {
-      if (error.message === 'Слишком коротко') {
-        alert("name и text должены содержать хотя бы 3 символа");
-        mainForm.classList.remove('-display-none');
-        loaderCommentsElement.classList.add('-display-none');
-
-      } else if (error.message === 'Сервер сломался') {
-        console.warn(error);
-        repeatedRequestComment();
-      }
-
-
-    });
+    text: commentsElement.value.replaceAll('<', '&lt;').replaceAll('>', '&gt;')
   })
-};
+    .then(responseData => {
+      comments = responseData
+    })
+    .then(() => {
+      return fetchGetAndRenderComments()
+    })
+    .then(data => {
+      mainForm.classList.remove('-display-none')
+      loaderCommentsElement.classList.add('-display-none')
 
-function renderApp() {
-  const appEl = document.getElementById("app");
-const commentsEl = document.getElementById("list");
-if (!token) {
-    
-  renderLoginComponent({
-    appEl, 
-    setToken: (newToken) => {
-    token = newToken;
-  },
-  renderApp,
-  
-});
-
-  // return;
+      nameElement.value = ''
+      commentsElement.value = ''
+      buttonElement.disabled = true
+    })
+    .catch(error => {
+      if (error.message === 'Слишком коротко') {
+        alert('name и text должены содержать хотя бы 3 символа')
+        mainForm.classList.remove('-display-none')
+        loaderCommentsElement.classList.add('-display-none')
+      }
+      if (error.message === 'Сервер сломался') {
+        repeatedRequestComment()
+        console.warn(error)
+      }
+    })
 }
 
-   commentsEl.innerHTML = comments.map((comment, index,) => {
-    return `<li class="comment" id = "comment-list" data-index="${index}">
+function addComments () {
+  const buttonElement = document.getElementById('add-button')
+  const nameElement = document.getElementById('name-input')
+  const commentsElement = document.getElementById('comments-input')
+  const mainForm = document.querySelector('.add-form')
+  const loaderCommentsElement = document.getElementById('loaderComments')
+  buttonElement.disabled = true
+
+  document.querySelectorAll('#name-input,#comments-input').forEach(element => {
+    element.addEventListener('input', () => {
+      if ( commentsElement.value === '') {
+        buttonElement.disabled = true
+      } else {
+        buttonElement.disabled = false
+      }
+    })
+  })
+
+  mainForm.addEventListener('keyup', e => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      buttonElement.click()
+    }
+  })
+  buttonElement.addEventListener('click', () => {
+      commentsElement.classList.remove('error')
+
+     if (commentsElement.value === '') {
+      commentsElement.classList.add('error')
+      return
+    }
+    mainForm.classList.add('-display-none')
+    loaderCommentsElement.classList.remove('-display-none')
+
+    // Передаем комментарий
+    return postComments({
+      token,
+      text: commentsElement.value
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+    })
+      .then(responseData => {
+        comments = responseData
+      })
+      .then(() => {
+        return fetchGetAndRenderComments()
+      })
+      .then(() => {
+        mainForm.classList.remove('-display-none')
+        loaderCommentsElement.classList.add('-display-none')
+
+        nameElement.value = ''
+        commentsElement.value = ''
+        buttonElement.disabled = true
+      })
+      .catch(error => {
+        if (error.message === 'Слишком коротко') {
+          alert('name и text должены содержать хотя бы 3 символа')
+          mainForm.classList.remove('-display-none')
+          loaderCommentsElement.classList.add('-display-none')
+        } else if (error.message === 'Сервер сломался') {
+          console.warn(error)
+          repeatedRequestComment()
+        }
+      })
+  })
+}
+
+function renderApp () {
+  const appEl = document.getElementById('app')
+
+  if (!token) {
+    renderLoginComponent({
+      comments,
+      appEl,
+      setToken: newToken => {
+        token = newToken
+      },
+      setUserName: newName =>{
+        userName = newName
+      },
+      
+      renderApp
+    })
+
+    return
+  }
+
+  const commentHtml = comments
+    .map((comment, index) => {
+      return `<li class="comment" id = "comment-list" data-index="${index}">
     <div class="comment-header">
       <div data-index="${index}">${comment.name}</div>
       <div>${comment.date} </div>
@@ -257,27 +253,53 @@ if (!token) {
     </div>
     <div class="comment-footer">
       <div class="likes">
-      <button id="delete-button" class = "delete-button" data-id ="${comment.id}">Удалить</button>
+      <button id="delete-button" class = "delete-button" data-id ="${
+        comment.id
+      }">Удалить</button>
         <span class="likes-counter">${comment.likeCount}</span>
-        <button data-index="${comment.id}" class="${comment.liked ? 'like-button -loading-like -active-like' : 'like-button'}"></button>
+        <button data-index="${index}" class="${
+        comment.liked ? 'like-button -loading-like -active-like' : 'like-button'
+      }"></button>
         
       </div>
     </div>
   </li>`
-  })
-    .join('');
+    })
+    .join('')
+
+  const appHtml = `<div class="container">
+    <ul class="comments" id="list" data>
+    ${commentHtml}
+      <!-- Рендеринг в JS -->
+    </ul>
+    <section id="loaderComments" class="loader -display-none">
+      <h4 class="loader-text" id="loaderText">Комментарии загружаются...</h4>
+      <div class="loader__gif">
+        <img src="./img/Spinner-1s-100px.svg" alt="loader">
+      </div>
+    </section>
+    <div class="add-form">
+      <input id="name-input" type="text" class="add-form-name" placeholder="${userName}"   />
+      <textarea id="comments-input" type="textarea" class="add-form-text" placeholder="Введите ваш коментарий"
+        rows="4"></textarea>
+      <div class="add-form-row">
+       <button class="add-form-button" id="add-button">Написать</button>
+       <button id="exit" class = "exit-button">Выйти</button>
+      </div>
+    </div>`
+  appEl.innerHTML = appHtml
+  document.getElementById('name-input').setAttribute('disabled', 'disabled');
  
-  
+
+   document.getElementById('exit').addEventListener('click', ()=>{
+    token = !token;
     
+    renderApp()
+  });
   
-  
-  
-  
-
-
-  ButtonTouch();
-  initTouchComment();
-  buttonDelete();
-  addComments();
-};
-
+// console.log(userName);
+  ButtonTouch()
+  initTouchComment()
+  buttonDelete()
+  addComments()
+}
